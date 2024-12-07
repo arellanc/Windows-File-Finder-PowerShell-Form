c3006d3305c57ba402c5c7d9238fdfac3dc47331
CJ Arellano <65259483+arellanc@users.noreply.github.com>
2024-12-07 15:19:08 -0500

    Update Launch Find your Files.ps1
    Updated environment username

--- File Finder Project/Launch Find your Files.ps1
+++ File Finder Project/Launch Find your Files.ps1
@@ -1,5 +1,5 @@
-﻿# Imports Module
-Import-Module 'C:\Users\Chall\Documents\File Finder Project\Find your Files Module.psm1'
+# Imports Module
+Import-Module 'C:\Users\$env:UserName\Documents\File Finder Project\Find your Files Module.psm1'
 
 # Launch the Find Files App
 Find-Files
