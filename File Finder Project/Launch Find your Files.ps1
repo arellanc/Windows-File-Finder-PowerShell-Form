0f1661ab2e27bbc197012f1b9d2ab2796b5ba7b7
Challan Arellano <arelcj0707@gmail.com>
2024-12-08 12:55:47 -0500

    Fixed file paths for user environmental variables

--- File Finder Project/Launch Find your Files.ps1
+++ File Finder Project/Launch Find your Files.ps1
@@ -1,5 +1,5 @@
 # Imports Module
-Import-Module 'C:\Users\$env:UserName\Documents\File Finder Project\Find your Files Module.psm1'
+Import-Module "C:\Users\$env:UserName\Documents\File Finder Project\Find your Files Module.psm1"
 
 # Launch the Find Files App
 Find-Files
